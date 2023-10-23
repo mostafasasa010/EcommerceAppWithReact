a378abf810ed57767241237a8a71d2695d5b7ee6
mostafasasa010 <mostafa.sasa20004@gmail.com>
2023-10-23 19:02:13 +0300

    Handle Variables

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+// Style File
+import "./Style/main.css";
+// App File
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
