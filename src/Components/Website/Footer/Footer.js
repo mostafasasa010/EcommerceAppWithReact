1f23c59cf07dc33f19b9ac4c66b0262caa0b0b32
mostafasasa010 <mostafa.sasa20004@gmail.com>
2023-10-26 01:16:02 +0300

    Started To Footer

--- src/Components/Website/Footer/Footer.js
+++ src/Components/Website/Footer/Footer.js
@@ -1,8 +1,23 @@
 function Footer() {
   return (
-    <header className="main-footer">
-      <h1>Footer</h1>
-    </header>
+    <footer className="main-footer">
+      <div className="container">
+        <div>
+          <div className="foot-one">
+            <div className="top">
+              <img src={require("../../../Imgs/Icon_Primary.png")} alt="Icon" />
+              <h3>CLICON</h3>
+            </div>
+            <p className="gray">Customer Supports:</p>
+            <p>{`(629) 555-0129`}</p>
+            <p className="gray">
+              4517 Washington Ave. Manchester, Kentucky 39495
+            </p>
+            <p>info@kinbo.com</p>
+          </div>
+        </div>
+      </div>
+    </footer>
   );
 }
 
