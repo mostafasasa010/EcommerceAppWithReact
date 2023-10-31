3f2e1197d36cfbdea8382ed05ba02d43156a0e8a
mostafasasa010 <mostafa.sasa20004@gmail.com>
2023-10-31 23:05:14 +0200

    Show Users In Dashboard

--- src/Pages/Dashboard/Users.js
+++ src/Pages/Dashboard/Users.js
@@ -1,37 +1,60 @@
+import { useEffect, useState } from "react";
+import Cookie from "cookie-universal";
+import { BaseApi, USER } from "../../API/Api";
+import Loading from "../../Components/Loading/Loading";
+
 function Users() {
+  const [usersData, setUsersData] = useState([]);
+  const cookie = Cookie();
+  const [loading, setLoading] = useState(true);
+  async function api() {
+    try {
+      const response = await fetch(`${BaseApi}${USER}`, {
+        headers: {
+          Authorization: "Bearer " + cookie.get("cookieToken"),
+        },
+      });
+      const data = await response.json();
+      setUsersData(data.data.data);
+      setLoading(false);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  useEffect(() => {
+    api();
+  }, []);
+  console.log(usersData);
+  function writeName() {
+    if (usersData && usersData.length > 0) {
+      return usersData.map((user, i) => (
+        <tr key={i}>
+          <td>{i}</td>
+          <td>{user.name}</td>
+          <td>{user.email}</td>
+          <td>{user.role}</td>
+        </tr>
+      ));
+    }
+    return null;
+  }
   return (
-    <div className="users">
-      <table>
-        <thead>
-          <tr>
-            <th>Id</th>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>1</td>
-            <td>Mostafa</td>
-            <td>Mo@gmail.com</td>
-            <td>Test</td>
-          </tr>
-          <tr>
-            <td>1</td>
-            <td>Mostafa</td>
-            <td>Mo@gmail.com</td>
-            <td>Test</td>
-          </tr>
-          <tr>
-            <td>1</td>
-            <td>Mostafa</td>
-            <td>Mo@gmail.com</td>
-            <td>Test</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
+    <>
+      {loading && <Loading />}
+      <div className="users">
+        <table>
+          <thead>
+            <tr>
+              <th>Id</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Role</th>
+            </tr>
+          </thead>
+          <tbody>{writeName()}</tbody>
+        </table>
+      </div>
+    </>
   );
 }
 
