7253fc67e1b043f10bc706b855b15c9a496e8330
mostafasasa010 <mostafa.sasa20004@gmail.com>
2023-11-04 01:07:11 +0200

    Fix A Bug From Mobile

--- src/Pages/Dashboard/ShowUser.js
+++ src/Pages/Dashboard/ShowUser.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { BaseApi, USER } from "../../API/Api";
 import Cookie from "cookie-universal";
 import Loading from "../../Components/Loading/Loading";
