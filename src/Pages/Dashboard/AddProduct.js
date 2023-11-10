48cb720b3b6b73ba16b4af3124e55522c4d4d8a5
mostafasasa010 <mostafa.sasa20004@gmail.com>
2023-11-10 17:22:10 +0200

    Finish Html Structure to Add Product Page

--- src/Pages/Dashboard/AddProduct.js
+++ src/Pages/Dashboard/AddProduct.js
@@ -4,6 +4,9 @@ import { BaseApi, CATEGORY } from "../../API/Api";
 
 function AddProduct() {
   const [categories, setCategories] = useState("");
+  const [discountOp, setdiscountOp] = useState("");
+  const [OriginalP, setOriginalP] = useState(0);
+  const [discountP, setdiscountP] = useState(0);
   async function getCategories() {
     const res = await axios.get(`${BaseApi}${CATEGORY}`);
     const data = res.data.data.data;
@@ -26,6 +29,27 @@ function AddProduct() {
       <input id="brand" name="brand" />
       <label htmlFor="stock">Stock Namber:</label>
       <input type="number" id="stock" name="stock" />
+      <div>
+        <label htmlFor="price">Price:</label>
+        <input
+          id="price"
+          placeholder="Original Price"
+          onChange={(e) => setOriginalP(e.target.value)}
+        />
+        <label htmlFor="discount">Discount</label>
+        <select id="discount" onChange={(e) => setdiscountOp(e.target.value)}>
+          <option value="No">No</option>
+          <option value="Yes">Yes</option>
+        </select>
+        {discountOp === "Yes" && (
+          <input
+            placeholder="Discount"
+            onChange={(e) => setdiscountP(e.target.value)}
+          />
+        )}
+        <label htmlFor="finalP">Final Price:</label>
+        <span id="finalP">{OriginalP - discountP}</span>
+      </div>
       <button type="submit">Add</button>
     </form>
   );
